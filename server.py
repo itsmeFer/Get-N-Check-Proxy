81b81d5b69c3562c1fe4686a576a5b602f1a409d
itsmeFer <ferdinandsianturi28@gmail.com>
2024-11-18 22:02:47 +0700

    .

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def pilih_bahasa():
     elif pilihan == '2':
         return "EN"
     else:
-        print("Pilihan tidak valid. Menggunakan bahasa Inggris sebagai default.")
+        print("Pilihan ini tidak valid. Menggunakan bahasa Inggris sebagai default.")
         return "EN"
 
 # Fungsi untuk mengambil proxy dari halaman
